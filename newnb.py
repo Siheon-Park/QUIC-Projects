4de3d551bcb66692779a5ea6d05c2656d345e1cc
Siheon Park <sihoney97@kaist.ac.kr>
2021-02-26 14:28:37 +0900

    notebook creation tools

--- newnb.py
+++ newnb.py
@@ -1,8 +1,12 @@
 import sys
 import os
 import time
+import shutil
+import pathlib
 
-def new_summary_notebook(base_name:str='swap_classifier'):
+QUIC_PATH = '/home/quic/QUIC-Projects'
+
+def new_summary_notebook(base_name:str='swap_classifier', sample_file:str='/home/quic/QUIC-Projects/sample_notebook.ipynb'):
     dir_name = time.strftime('%b %d %a', time.localtime(time.time()))
     today_ = time.strftime('%b%d', time.localtime(time.time()))
     dir_path = os.path.join(os.getcwd(), 'summary_ipynb', dir_name)
@@ -12,13 +16,15 @@ def new_summary_notebook(base_name:str='swap_classifier'):
         os.mkdir(os.path.join(dir_path, 'figs'))
     filename = '{:}({:}).ipynb'.format(base_name, today_)
     if not os.path.isfile(os.path.join(dir_path, filename)):
-        with open(os.path.join(dir_path,filename), 'a') as f:
-            pass
+        shutil.copy(sample_file, os.path.join(dir_path, filename))
+        #with open(os.path.join(dir_path,filename), 'a') as f:
+            #pass
 
-quic_path = '/home/quic/QUIC-Projects'
     
 if __name__=='__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) == 1:
         new_summary_notebook()
-    else:
+    elif len(sys.argv) == 2:
         new_summary_notebook(sys.argv[1])
+    else:
+        new_summary_notebook(sys.argv[1], sys.argv[2])
