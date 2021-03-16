845ad06f60d1811a6ffc8c65f9208ef7ac6723cf
Siheon Park <sihoney97@kaist.ac.kr>
2021-03-16 23:24:52 +0900

    added logger, now import utils and ConvexError

--- classifiers/convex/svm.py
+++ classifiers/convex/svm.py
@@ -1,47 +1,30 @@
 import numpy as np
+import cvxopt
 from matplotlib import pyplot as plt
 from sklearn.metrics import accuracy_score
+import logging
 from ._cvxopt_helpers_ import _Matrix_Helper
+from . import ConvexError
+from .. import process_info_from_alpha, Classifier
 from ..kernel import Kernel
 
-import cvxopt
-
 _EPS = 1e-5
-        
-class Classifier:
-    def __init__(self, data:np.ndarray, label:np.ndarray):
-        self.data = data
-        self.label = label
-        self.num_data = data.shape[0]
-        self.dim_data = data.shape[1]
-        self.alpha = None
-        self.name = type(self).__name__
-        if self.num_data != label.size:
-            raise ValueError('Not enough/More number of labels compare to dataset')
 
+logger = logging.getLogger(__name__)
+        
 class BinarySVM(Classifier):
     def __init__(self, kernel:Kernel, C:float=None, mutation:str='SVM', **kwargs)->None:
         self.kernel = kernel
         self.C = C
         _options = ['SVM', 'QASVM', 'REDUCED_SVM', 'REDUCED_QASVM', 'REDUCED_primal_SVM', 'REDUCED_primal_QASVM', 'uniform_QASVM', 'REDUCED_uniform_QASVM']
         if mutation not in _options:
-            raise ValueError('Expect one of {:}, received {:}'.format(_options, mutation))
+            raise ConvexError('Expect one of {:}, received {:}'.format(_options, mutation))
         if 'REDUCED' in mutation:
             self.k = kwargs.get('k', 1)
         self.mutation = mutation
         self.status=None
         self.iterations=None
         self.alpha = None
-
-    def __repr__(self) -> str:
-        str_list=[]
-        str_list.append(f'BinarySVM: ({self.mutation})')
-        str_list.append(f'\n\tKernel: {self.kernel}')
-        str_list.append(f'\n\tHyperParameter: {self.C}')
-        str_list.append(f'\n\tOptimization Status: {self.status}')
-        str_list.append(f'\n\tIterations: {self.iterations}')
-        str_list.append('\n')
-        return ''.join(str_list)
 
     def f(self, test:np.ndarray):
         if len(test.shape)==1:
@@ -56,6 +39,7 @@ class BinarySVM(Classifier):
         return accuracy_score(self.predict(test), testlabel)
 
     def fit(self, X:np.ndarray, y:np.ndarray)->None:
+        logger.debug(repr(self))
         super().__init__(X, y)
         self.polary = 2*y-1
         if 'uniform' in self.mutation:
@@ -63,8 +47,23 @@ class BinarySVM(Classifier):
         else:
             self._fit_cvxopt()
         self._fit_postprocessing()
+        logger.debug(repr(self))
+
+    def __repr__(self) -> str:
+        str_list=[]
+        str_list.append(str(self))
+        str_list.append(f'Kernel: {self.kernel}')
+        str_list.append(f'HyperParameter: {self.C}')
+        str_list.append(f'Optimization Status: {self.status}')
+        str_list.append(f'Iterations: {self.iterations}')
+        str_list.append('\n')
+        return '\n\t'.join(str_list)
+
+    def __str__(self) -> str:
+        return f'BinarySVM: ({self.mutation})'
 
     def _fit_cvxopt(self):
+        logger.info('CVXOPT fitting starting')
         K, Y = _Matrix_Helper.__find_kernel_matrix__(self.data, self.polary, self.kernel)
         if 'REDUCED' in self.mutation:
             P = (K+1/self.k)*Y
@@ -100,6 +99,7 @@ class BinarySVM(Classifier):
             self.alpha = self.alpha[:self.num_data]
             
     def _fit_uniform(self):
+        logger.info('CVXOPT fitting skipped for being uniform svm')
         self.status = 'uniform'
         self.iterations = 0
         self.alpha = np.ones(self.num_data)/self.num_data
@@ -119,9 +119,8 @@ class BinarySVM(Classifier):
                 self.b = b
         else:
             self.b = np.sum(self.alpha*self.polary)/self.k
+        logger.debug('bias b for {:}: {:.10f}'.format(self.mutation, self.b))
 
-        self.support_ = np.argwhere(self.alpha>0.01*max(self.alpha)).flatten()
-        self.support_vectors_ = self.data[self.support_]
-        self.n_support_ = len(self.support_)
+        self.support_, self.support_vectors_, self.n_support_ = process_info_from_alpha(self.alpha, self.data)
 
 
