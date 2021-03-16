ccb2867435a47005427059ff1f5563a461f330cf
Siheon Park <sihoney97@kaist.ac.kr>
2021-03-16 23:30:05 +0900

    logger added, fix miner bugs

--- classifiers/quantum/quantum_circuits.py
+++ classifiers/quantum/quantum_circuits.py
@@ -1,12 +1,13 @@
-
+import logging
 import numpy as np
 import math
 from qiskit.circuit.parameter import Parameter
 from qiskit.circuit.parametervector import ParameterVector
-from qiskit import execute
 from qiskit.circuit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit.circuit.library import XGate, U3Gate
-from typing import Union, Optional, Dict, List, Callable
+from typing import Union, List
+
+logger = logging.getLogger(__name__)
 
 class QASVM_circuit(QuantumCircuit):
     """ 
@@ -65,11 +66,19 @@ class QASVM_circuit(QuantumCircuit):
         self.compose(var_form, qubits=list(self._reg_dict[reg]), inplace=True)
 
 class uniform_QASVM_circuit(QASVM_circuit):
+    """ not using var_form """
     def add_var_form(self, var_form, reg:str='i'):
+        if var_form is not None:
+            logger.warning("ignoring {:} for {:} is not using it.".format('var_form', self.__class__.__name__))
         self.h(self._reg_dict[reg])
 
 class Bloch_sphere_QASVM_circuit(QASVM_circuit):
+    """ not using feature_map, feature_map_params """
     def UD_encode(self, feature_map, feature_map_params, training_data:np.ndarray, training_label:np.ndarray, N:int, reg:str='i'):
+        if feature_map is not None:
+            logger.warning("ignoring {:} for {:} is not using it.".format('feature_map', self.__class__.__name__))
+        if feature_map_params is not None:
+            logger.debug("ignoring {:} for {:} is not using it.".format('feature_map_params', self.__class__.__name__))
         ctrl_q = self._reg_dict[reg]
         target_q = self._reg_dict['x'+reg]
         target_y = self._reg_dict['y'+reg]
@@ -78,14 +87,38 @@ class Bloch_sphere_QASVM_circuit(QASVM_circuit):
         self.ucrz([phi for phi in training_data[:,1]], ctrl_q, target_q)
         self.ucrx(list(np.where(training_label>0.5, 0, np.pi)), ctrl_q, target_y)
 
-class Bloch_uniform_QASVM_circuit(Bloch_sphere_QASVM_circuit, uniform_QASVM_circuit):
-    def add_var_form(self, var_form, reg: str):
-        self.h(self._reg_dict[reg])
+    def X_encode(self, feature_map, feature_map_params, testdata:Union[np.ndarray, ParameterVector, List[Parameter]], reg:str='xj'):
+        if feature_map is not None:
+            logger.warning("ignoring {:} for {:} is not using it.".format('feature_map', self.__class__.__name__))
+        if feature_map_params is not None:
+            logger.debug("ignoring {:} for {:} is not using it.".format('feature_map_params', self.__class__.__name__))
+        temp_feature_map_params = ParameterVector('X', 2)
+        temp_feature_map = QuantumCircuit(1)
+        temp_feature_map.u3(temp_feature_map_params[0], temp_feature_map_params[1], 0, 0)
+        feature_map_param_dict = {p:v for p, v in zip(temp_feature_map_params, testdata)}
+        self.compose(temp_feature_map.assign_parameters(feature_map_param_dict), qubits=list(self._reg_dict[reg]), inplace=True)
+
+class Bloch_uniform_QASVM_circuit(Bloch_sphere_QASVM_circuit):
+    """ not using var_form, feature_map, feature_map_params """
+    add_var_form = uniform_QASVM_circuit.add_var_form
 
 class _uc_QASVM_circuit(QASVM_circuit):
     def UD_encode(self, feature_map, feature_map_params, training_data:np.ndarray, training_label:np.ndarray, N:int, reg:str='i'):
+        if feature_map is not None:
+            logger.warning("ignoring {:} for {:} is not using it.".format('feature_map', self.__class__.__name__))
+        if feature_map_params is not None:
+            logger.debug("ignoring {:} for {:} is not using it.".format('feature_map_params', self.__class__.__name__))
         ctrl_q = self._reg_dict[reg]
         target_q = self._reg_dict['x'+reg]
         target_y = self._reg_dict['y'+reg]
         self.uc([U3Gate(theta, phi, 0).to_matrix() for theta, phi in training_data], ctrl_q, target_q)
         self.ucrx(list(np.where(training_label>0.5, 0, np.pi)), ctrl_q, target_y)
+
+# global variables
+_CIRCUIT_CLASS_DICT = {
+    'QASVM': QASVM_circuit,
+    'Bloch_sphere': Bloch_sphere_QASVM_circuit,
+    'uniform': uniform_QASVM_circuit,
+    'Bloch_uniform': Bloch_uniform_QASVM_circuit,
+    '_uc': _uc_QASVM_circuit
+}
