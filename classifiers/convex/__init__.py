3fdbcad38332feeaeb9925019fd28c3c5dc23569
Siheon Park <sihoney97@kaist.ac.kr>
2021-03-16 23:25:30 +0900

    ConvexError is defined

---
+++ classifiers/convex/__init__.py
@@ -0,0 +1,7 @@
+class ConvexError(BaseException):
+    def __init__(self, *message: object) -> None:
+        self.message = ' '.join(message)
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return repr(self.message)
