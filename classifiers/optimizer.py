0b32ab0e412a56a7923b52dddda9af3d2b112bc8
Siheon Park <sihoney97@kaist.ac.kr>
2021-04-03 02:14:36 +0900

    support for dictionary input

--- classifiers/optimizer.py
+++ classifiers/optimizer.py
@@ -1,22 +1,19 @@
 from abc import ABC, abstractmethod
-from typing import Callable
+from typing import Callable, Dict, Union
 import logging
 import numpy as np
 from qiskit.aqua import aqua_globals
+from qiskit.circuit.parameter import Parameter
 from qiskit.circuit.parametervector import ParameterVector
 
 logger = logging.getLogger(__name__)
 
 class StocasticOptimizer(ABC):
-    def __init__(self, objective:Callable, params:ParameterVector, hyperparams:dict, initial_point:np.ndarray=None) -> None:
+    def __init__(self, objective:Callable, params:Union[Dict[Parameter, float], np.ndarray], **hyperparams:dict) -> None:
         super().__init__()
         self.objective = objective
         self.hyperparams = hyperparams
-        self.initial_point=initial_point
-        if self.initial_point is None:
-            self.params = {p:None for p in params}
-        else:
-            self.params = dict(zip(params, initial_point))
+        self.params = params
 
     @abstractmethod
     def step(self):
@@ -24,29 +21,26 @@ class StocasticOptimizer(ABC):
         raise NotImplementedError
 
 class SPSA(StocasticOptimizer):
-    def __init__(self, objective:Callable, params:ParameterVector,
+    def __init__(self, objective:Callable, 
+                 params:Union[Dict[Parameter, float], np.ndarray],
                  c0: float = 2 * np.pi * 0.1,
                  c1: float = 0.1,
                  c2: float = 0.602,
                  c3: float = 0.101,
-                 c4: float = 0,
-                 initial_point: np.ndarray = None) -> None:
-        hyperparams = dict(c0=c0, c1=c1, c2=c2, c3=c3, c4=c4)
-        if initial_point is None:
-            initial_point = np.pi*(2*np.random.rand(len(params))-1)
-        super().__init__(objective, params, hyperparams, initial_point)
+                 c4: float = 0) -> None:
+        super().__init__(objective, params, c0=c0, c1=c1, c2=c2, c3=c3, c4=c4)
         self.k=0
 
     def step(self, k:int=None):
         """ evolve SPSA """
-        theta = np.array(list(self.params.values()))
+        theta = self.params
         if k is not None:
             self.k = k
         # SPSA Parameters
         a_spsa = float(self.hyperparams['c0']) / np.power(self.k + 1 + self.hyperparams['c4'],
                                                         self.hyperparams['c2'])
         c_spsa = float(self.hyperparams['c1']) / np.power(self.k + 1, self.hyperparams['c3'])
-        delta = 2 * aqua_globals.random.integers(2, size=np.shape(theta)[0]) - 1
+        delta = 2 * aqua_globals.random.integers(2, size=len(theta)) - 1
         # plus and minus directions
         theta_plus = theta + c_spsa * delta
         theta_minus = theta - c_spsa * delta
@@ -59,8 +53,7 @@ class SPSA(StocasticOptimizer):
         theta = theta - a_spsa * g_spsa
         logger.debug('Objective function at theta+ for step # %s: %1.7f', self.k, cost_plus)
         logger.debug('Objective function at theta- for step # %s: %1.7f', self.k, cost_minus)
-        for i, k in enumerate(self.params.keys()):
-            self.params[k] = theta[i]
+        self.params = theta
         self.k+=1
 
     def calibrate(self, maxiter:int=1000):
@@ -73,7 +66,7 @@ class SPSA(StocasticOptimizer):
 
         c1 is initial_c and is first perturbation of initial_theta.
         """
-        initial_theta = np.array(list(self.params.values()))
+        initial_theta = self.params
         num_steps_calibration = min(25, max(1, maxiter // 5))
         target_update = self.hyperparams['c0']
         initial_c = self.hyperparams['c1']
@@ -82,7 +75,7 @@ class SPSA(StocasticOptimizer):
         for i in range(num_steps_calibration):
             if i % 5 == 0:
                 logger.debug('calibration step # %s of %s', str(i), str(num_steps_calibration))
-            delta = 2 * aqua_globals.random.integers(2, size=np.shape(initial_theta)[0]) - 1
+            delta = 2 * aqua_globals.random.integers(2, size=len(initial_theta)) - 1
             theta_plus = initial_theta + initial_c * delta
             theta_minus = initial_theta - initial_c * delta
             obj_plus = self.objective(theta_plus)
