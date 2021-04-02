76d586da50e834d8f6b8e8c5170ac8e9b889318e
Siheon Park <sihoney97@kaist.ac.kr>
2021-04-03 02:15:07 +0900

    now also make README.md

--- newnb.py
+++ newnb.py
@@ -1,25 +1,25 @@
 import sys
-import os
 import time
 import shutil
-import pathlib
+from pathlib import Path
 
 QUIC_PATH = '/home/quic/QUIC-Projects'
 
-def new_summary_notebook(base_name:str='swap_classifier', sample_file:str='/home/quic/QUIC-Projects/sample_notebook.ipynb'):
-    dir_name = time.strftime('%b %d %a', time.localtime(time.time()))
-    today_ = time.strftime('%b%d', time.localtime(time.time()))
-    dir_path = os.path.join(os.getcwd(), 'summary_ipynb', dir_name)
-    if not os.path.isdir(dir_path):
-        os.mkdir(dir_path)
-    if not os.path.isdir(os.path.join(dir_path, 'figs')):
-        os.mkdir(os.path.join(dir_path, 'figs'))
+def new_summary_notebook(base_name:str='swap_classifier', sample_file:str=Path(QUIC_PATH)/'sample_notebook.ipynb'):
+    local_time = time.localtime(time.time())
+    year = time.strftime('%Y', local_time)
+    month = time.strftime('%m %B', local_time)
+    day = time.strftime('%d %a', local_time)
+    today_ = time.strftime('%b%d', local_time)
+    dir_path = Path(QUIC_PATH)/'summary_ipynb'/year/month/day
+    (dir_path/'figs').mkdir(parents=True, exist_ok=True)
     filename = '{:}({:}).ipynb'.format(base_name, today_)
-    if not os.path.isfile(os.path.join(dir_path, filename)):
-        shutil.copy(sample_file, os.path.join(dir_path, filename))
-        #with open(os.path.join(dir_path,filename), 'a') as f:
-            #pass
-
+    if not (dir_path/filename).is_file():
+        shutil.copy(sample_file, dir_path/filename)
+    README_txt = dir_path/'README.txt'
+    if not (dir_path/'README.md').is_file():
+        with README_txt.open('w') as f: f.write('# Summary of {:}\n'.format(time.strftime('%A %B %d', local_time)))
+        README_txt.rename(README_txt.with_suffix('.md'))
     
 if __name__=='__main__':
     if len(sys.argv) == 1:
