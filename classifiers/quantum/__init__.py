468b9927b99d1b687d2f524ef8c672c5c61eb78f
Siheon Park <sihoney97@kaist.ac.kr>
2021-05-05 00:57:20 +0900

    checkout quantum.__init__.py

--- classifiers/quantum/__init__.py
+++ classifiers/quantum/__init__.py
@@ -5,6 +5,7 @@ from .. import Classifier
 from qiskit.circuit import QuantumRegister
 from qiskit.circuit import Qubit
 from qiskit.aqua import AquaError
+from qiskit.transpiler import Layout
 from qiskit.providers.basebackend import BaseBackend
 from itertools import product
 from typing import Union, Dict
@@ -14,33 +15,38 @@ logger = logging.getLogger(__name__)
 class QuantumClassifier(Classifier):
     pass
 
-class Qasvm_Mapping_4x2(object):
+class Qasvm_Mapping_4x2(Layout):
     """ order: a, i0, i1, xi, yi, j0, j1, xj, yj """
 
-    def __init__(self, backend:Union[str, BaseBackend]):
-        self.backend_name = backend if isinstance(backend, str) else backend.name()
-        self.updated_date = None
-
-    @property
-    def layout(self):
-        QUBIT_LISTS = [Qubit(QuantumRegister(1, 'a'), 0),
-                        Qubit(QuantumRegister(2, 'i'), 0),
-                        Qubit(QuantumRegister(2, 'i'), 1),
-                        Qubit(QuantumRegister(1, 'xi'), 0),
-                        Qubit(QuantumRegister(1, 'yi'), 0),
-                        Qubit(QuantumRegister(2, 'j'), 0),
-                        Qubit(QuantumRegister(2, 'j'), 1),
-                        Qubit(QuantumRegister(1, 'xj'), 0),
-                        Qubit(QuantumRegister(1, 'yj'), 0)]
-        if 'sydney' in self.backend_name:
-            self.updated_date = '2021/03/28 02:41'
-            prefered_mapping_order = [14, 5, 9, 11, 8, 20, 22, 16, 19]
-        elif 'toronto' in self.backend_name:
-            self.updated_date = '2021/03/21 15:38'
-            prefered_mapping_order = [14, 5, 9, 11, 8, 20, 22, 16, 19]
+    def __init__(self, backend:Union[str, BaseBackend, dict]=None):
+        if backend is None or isinstance(backend, dict):
+            super().__init__(backend)
         else:
-            raise QuantumError('No support for {:}'.format(self.backend_name))
-        return {QUBIT_LISTS[i]:prefered_mapping_order[i] for i in range(len(QUBIT_LISTS))}
+            self._backend_name = backend if isinstance(backend, str) else backend.name()
+            self._QUBIT_LISTS = [Qubit(QuantumRegister(1, 'a'), 0),
+                            Qubit(QuantumRegister(2, 'i'), 0),
+                            Qubit(QuantumRegister(2, 'i'), 1),
+                            Qubit(QuantumRegister(1, 'xi'), 0),
+                            Qubit(QuantumRegister(1, 'yi'), 0),
+                            Qubit(QuantumRegister(2, 'j'), 0),
+                            Qubit(QuantumRegister(2, 'j'), 1),
+                            Qubit(QuantumRegister(1, 'xj'), 0),
+                            Qubit(QuantumRegister(1, 'yj'), 0)]
+            if 'sydney' in self._backend_name:
+                self.updated_date = '2021/04/19 01:12'
+                self._prefered_mapping_order = [23, 15, 17, 21, 18, 26, 22, 24, 25]
+            elif 'toronto' in self._backend_name:
+                self.updated_date = '2021/05/04 23:16'
+                self._prefered_mapping_order = [7, 0, 2, 4, 1, 13, 15, 10, 12]
+            else:
+                raise QuantumError('No support for {:}'.format(self._backend_name))
+            super().__init__(dict(zip(self._QUBIT_LISTS, self._prefered_mapping_order)))
+    
+    @property
+    def _layout_for_first_order_circuit(self):
+        _vqs = self._QUBIT_LISTS[:5]+self._QUBIT_LISTS[7:8]
+        _pqs = self._prefered_mapping_order[:5]+self._prefered_mapping_order[7:8]
+        return Qasvm_Mapping_4x2(dict(zip(_vqs, _pqs)))
 
 def postprocess_Z_expectation(n:int, dic:Dict[str, float], *count):
     ''' interpretaion of qiskit result. a.k.a. parity of given qubits 'count' '''
